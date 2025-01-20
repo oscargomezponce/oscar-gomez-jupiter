f2efe73809f681f251d45852753f38fa321b8cb9
Oscar Gomez <oscargomezponce@gmail.com>
2025-01-20 18:09:35 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -15,3 +15,39 @@ skills.forEach((skillText) => {
     skill.innerText = skillText;
     skillsList.appendChild(skill);
   });
+
+  const messageForm = document.querySelector("form[name='leave_message']");
+
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+  
+    console.log("Name:", usersName);
+    console.log("Email:", usersEmail);
+    console.log("Message:", usersMessage);
+  
+    const messageSection = document.querySelector("#messages");
+    const messageList = messageSection.querySelector("ul");
+  
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `
+      <a href="mailto:${usersEmail}">${usersName}</a>: 
+      <span>${usersMessage}</span>
+    `;
+  
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+  
+    removeButton.addEventListener("click", () => {
+      messageList.removeChild(newMessage);
+    });
+  
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+  
+    messageForm.reset();
+  });
